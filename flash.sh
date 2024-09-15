8bff4e229be034258fc6613b6e8580fd54ec42b1
makerbase <makerbase@makerbase.cn>
2024-09-15 19:01:34 +0200

    Adds flash script

---
+++ flash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp .config_stm32f401 .config
+FLASH_DEVICE=$(find /dev/serial/by-id/ -name 'usb-Klipper_*')
+echo Flashing STM32 \(${FLASH_DEVICE}\)
+make clean && make all && sudo make flash FLASH_DEVICE=$FLASH_DEVICE
+cp .config_linux_process .config
+echo Flashing Linux Process
+make clean && sudo systemctl stop klipper makerbase-client && make all && sudo make flash && sudo systemctl start klipper makerbase-client
