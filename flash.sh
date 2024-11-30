586175b37bdfd94bfbf530b62888ba86ea1d8d8b
Gordin <9ordin@gmail.com>
2024-11-30 19:20:35 +0100

    makes flash script work while EddyUSB is connected

--- flash.sh
+++ flash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp .config_stm32f401 .config
-FLASH_DEVICE=$(find /dev/serial/by-id/ -name 'usb-Klipper_*')
+FLASH_DEVICE=$(find /dev/serial/by-id/ -name 'usb-Klipper_stm32f401xc*')
 echo "Flashing STM32 (${FLASH_DEVICE})"
 make clean && make all && sudo make flash FLASH_DEVICE="$FLASH_DEVICE"
 cp .config_linux_process .config
